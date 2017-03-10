d822822bd540a517a2cedf25c1e88ecdcf4af380
Jim Rohrer <jim.rohrer@gmail.com>
2017-03-09 20:38:11 -0800

    Add User model with validations, install bcrypt, and update routes

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap-sass'
 gem 'sprockets-rails', :require => 'sprockets/railtie'
+# bcrypt is used for encrypting User passwords
+gem 'bcrypt'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # get 'posts/new'    # etc...
   # get 'posts/edit'
 
+  resources :users, only: [:new, :create]
+
   get 'about' => 'welcome#about'  # allow users to visit '/about', rather than '/welcome/about'
   get 'faq' => 'welcome#faq'    # allow users to visit '/faq', rather than '/welcome/faq'
   # Above two entries shorten the URI while still mapping to the welcome controller's actions
